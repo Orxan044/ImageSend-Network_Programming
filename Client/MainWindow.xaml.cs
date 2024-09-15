f60f714e73677d0a0ee165c60c2933a995a3b42f
Orxan Huseynov <115917125+Orxan044@users.noreply.github.com>
2024-09-15 15:27:09 +0400

    :

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Client;
+
+
+public partial class MainWindow : Window
+{
+    string filePath;
+
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        var openFileDialog = new Microsoft.Win32.OpenFileDialog();
+        if (openFileDialog.ShowDialog() == true)
+        {
+            filePath = openFileDialog.FileName;
+            SelectedImage.Text = filePath;
+        }
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+        var task = Task.Run(() =>
+        {
+            try
+            {
+                Dispatcher.Invoke(() => 
+                { 
+                    var imageBytes = File.ReadAllBytes(filePath);
+                    var base64Image = Convert.ToBase64String(imageBytes);
+
+                    using (var client = new TcpClient(SelectedIp.Text, int.Parse(SelectedPort.Text)))
+                    using (var networkStream = client.GetStream())
+                    using (var writer = new StreamWriter(networkStream, Encoding.UTF8))
+                    {
+                        writer.Write(base64Image);
+                        writer.Flush();
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                Dispatcher.Invoke(() => MessageBox.Show(ex.Message));
+            }
+        }); 
+    }
+}
+
+
---
+++ Server/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Windows;
+using System.IO;
+using System.Text;
+using System.Windows.Media.Imaging;
+
+namespace Server;
+
+
+public partial class MainWindow : Window
+{
+    static int _port = 27001;
+    static IPAddress _ip = IPAddress.Parse("192.168.1.8");
+    private TcpListener? _listener;
+
+    static IPEndPoint _endpoint = new(_ip, _port);
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = this;
+        Start();
+    }
+
+
+    private async void Start()
+    {
+        try
+        {
+            _listener = new TcpListener(_ip, _port);
+            _listener.Start();
+            Dispatcher.Invoke(() => ListBoxInfarmation.Items.Add($"Listening on: {_listener.LocalEndpoint}"));
+
+            while (true)
+            {
+                var client = await _listener.AcceptTcpClientAsync();
+                HandleClient(client);
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+
+    }
+
+    private async void HandleClient(TcpClient client)
+    {
+        try
+        {
+            Dispatcher.Invoke(() => ListBoxInfarmation.Items.Add($"{client.Client.RemoteEndPoint} connected"));
+            using var networkRead = client.GetStream();
+            using (var reader = new StreamReader(networkRead, Encoding.UTF8))
+            {
+                var baseImage = await reader.ReadToEndAsync();
+                var image = BitmapImageFromBase64(baseImage);
+                Dispatcher.Invoke(() => ListBoxImages.Items.Add(image));
+            };
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+
+
+    }
+
+    private BitmapImage BitmapImageFromBase64(string base64Image)
+    {
+        try
+        {
+            var imageBytes = Convert.FromBase64String(base64Image);
+            var imageStream = new MemoryStream(imageBytes);
+
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = imageStream;
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapImage.EndInit();
+            bitmapImage.Freeze();
+
+            return bitmapImage;
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+            return null!;
+        }
+
+
+    }
+}
